fc052ec62cdf4442da63d36ca81acace8e70838f
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-24 18:55:50 +0800

    license processor implementation

---
+++ src/Packfire/Concrete/Processor/License.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Packfire Concrete
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Packfire\Concrete\Processor;
+
+/**
+ * A processor that lets the file show distinctively in the source code of the PHAR
+ * 
+ * @author Sam-Mauris Yong <sam@mauris.sg>
+ * @copyright 2012 Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * @license http://www.opensource.org/licenses/BSD-3-Clause The BSD 3-Clause License
+ * @package \Packfire\Concrete\Processor
+ * @since 1.0.0
+ * @link https://github.com/packfire/concrete
+ */
+
+class License implements IProcessor {
+    
+    public function process($source){
+        return "\n\n$source\n\n";
+    }
+    
+}
