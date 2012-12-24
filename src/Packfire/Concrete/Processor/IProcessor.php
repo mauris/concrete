9f7f510bfcbe40aef2f2b0fe26c619553d558449
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-24 18:47:43 +0800

    processor abstraction

---
+++ src/Packfire/Concrete/Processor/IProcessor.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Packfire Concrete
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Packfire\Concrete\Processor;
+
+/**
+ * Provides an interface for processors
+ * 
+ * @author Sam-Mauris Yong <sam@mauris.sg>
+ * @copyright 2012 Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * @license http://www.opensource.org/licenses/BSD-3-Clause The BSD 3-Clause License
+ * @package \Packfire\Concrete\Processor
+ * @since 1.0.0
+ * @link https://github.com/packfire/concrete
+ */
+
+interface IProcessor {
+    
+    public function process($source);
+    
+}
